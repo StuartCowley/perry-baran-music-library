e3d8fba06c013c79322af3d8fd42872344d3d837
Perry <petreli1@ymail.com>
2022-10-07 11:21:45 +0100

    added safeguards to prevent SQL injection into artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -5,9 +5,14 @@ exports.post = async (req, res) => {
     const { name, genre } = req.body;
 
     try {
-        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        await db.query(`INSERT INTO Artist (name, genre) VALUES (?, ?)`, [
+            name,
+            genre
+        ]);
         res.status(201).send();
     } catch (err) {
         res.status(500).json(err);
     }
+
+    db.close();
 };
