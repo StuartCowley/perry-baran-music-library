2d21d50fe281768ccdbc734792df35d9c387a181
Perry <petreli1@ymail.com>
2022-10-13 15:58:09 +0100

    prettier + try catch added to create database function

--- src/services/db.js
+++ src/services/db.js
@@ -3,13 +3,17 @@ const mysql = require('mysql2/promise');
 const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
 
 module.exports = async () => {
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_NAME,
-  });
+  try {
+    const connection = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+      database: DB_NAME,
+    });
 
-  return connection;
+    return connection;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
